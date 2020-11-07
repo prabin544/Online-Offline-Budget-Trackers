8f25c77dddf1119e931c9fe1126ff74c56b439f1
Prabin Singh <pravin544@gmail.com>
2020-11-07 15:03:43 -0600

    Deploying

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,59 +1,60 @@
-const filesToCache = [
+const FILES_TO_CACHE = [
     "/",
-    "/index.html",
+    "/index.js",
     "/manifest.json",
-    "/style.css",
-    "/icons/icon-152x152.png",
+    "/styles.css",
+    "/icons/icon-512x512.png",
     "/icons/icon-192x192.png",
 ];
 
-const staticCacheName = 'static-cache-v1';
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
 
-self.addEventListener('install', event => {
-console.log('Attempting to install service worker and cache static assets');
-event.waitUntil(
-    caches.open(staticCacheName)
-    .then(cache => {
-    return cache.addAll(filesToCache);
-    })
-);
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
 });
 
-self.addEventListener("activate", function(evt) {
-    evt.waitUntil(
-      caches.keys().then(keyList => {
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+      })
+      .then((cachesToDelete) => {
         return Promise.all(
-          keyList.map(key => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              console.log("Removing old cache data", key);
-              return caches.delete(key);
-            }
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
           })
         );
       })
-    );
-    self.clients.claim();
+      .then(() => self.clients.claim())
+  );
 });
 
-//fetch
-self.addEventListener('fetch', event => {
-console.log('Fetch event for ', event.request.url);
-event.respondWith(
-    caches.match(event.request)
-    .then(response => {
-    if (response) {
-        console.log('Found ', event.request.url, ' in cache');
-        return response;
-    }
-    console.log('Network request for ', event.request.url);
-    return fetch(event.request)
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
 
-    // TODO 4 - Add fetched files to the cache
-
-    }).catch(error => {
-
-    // TODO 6 - Respond with custom offline page
-
-    })
-);
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
 });
